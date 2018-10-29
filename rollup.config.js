027445f2e0ceea93e499c737d1d4ac871e5382de
but0n <but0n@icloud.com>
2018-10-29 10:23:46 +0800

    rename source -> src

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,9 @@
 import typescript from 'rollup-plugin-typescript2'
 import pkg from './package.json'
 export default {
-    input: 'source/index.ts',
+    input: 'src/index.ts',
     output: {
         format: 'umd',
-        name: 'THHHHHHHH',
         file: 'build/ashes.main.js',
     },
     plugins: [
