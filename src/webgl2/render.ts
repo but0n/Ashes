76bdec9c09b994d368c0965a940c4fb28f3e12d2
but0n <but0n@icloud.com>
2018-11-01 00:50:36 +0800

    uniform + attribute

--- src/webgl2/render.ts
+++ src/webgl2/render.ts
@@ -12,7 +12,7 @@ export class Render {
             console.error('Get Context Failed');
             return;
         }
-        this.setViewPort(); // initial - full screen
+        this.setScreenSize(); // initial - full screen
     }
 
     setScreenSize(width = window.innerWidth, height = window.innerHeight) {
@@ -56,10 +56,88 @@ export class Render {
 
     pickupActiveAttributes(shader: WebGLProgram) {
         const amount = this.gl.getProgramParameter(shader, this.gl.ACTIVE_ATTRIBUTES);
-        let attributes = {};
+        let attributes: Attribute[] = [];
         for(let i = 0; i < amount; i++) {
             const {name} = this.gl.getActiveAttrib(shader, i);
             const location = this.gl.getAttribLocation(shader, name);
+            attributes.push(new Attribute(name, location));
+        }
+        return attributes;
+    }
+
+    pickupActiveUniforms(shader: WebGLProgram) {
+        const amount = this.gl.getProgramParameter(shader, this.gl.ACTIVE_ATTRIBUTES);
+        let uniforms: Uniform[] = [];
+        for(let i = 0; i < amount; i++) {
+            const {name, type} = this.gl.getActiveUniform(shader, i);
+            const location = this.gl.getUniformLocation(shader, name);
+            uniforms.push(new Uniform(name, location, type));
+        }
+        return uniforms;
+    }
+}
+
+class Attribute {
+    name: string;
+    location: number;
+    constructor(name: string, location: number) {
+        this.name = name;
+        this.location = location;
+    }
+}
+
+class Uniform {
+    name: string;
+    location: WebGLUniformLocation;
+    type: GLenum;
+    setter: string;
+    constructor(name: string, location: WebGLUniformLocation, type: GLenum) {
+        this.name = name;
+        this.location = location;
+        this.type = type;
+        this.setter = Uniform.getUnifSetter(type);
+    }
+
+    static getUnifSetter(type: GLenum) {
+        switch(type) {
+            case WebGLRenderingContext.FLOAT:
+                return 'uniform1f';
+            case WebGLRenderingContext.FLOAT_VEC2:
+                return 'uniform2f';
+            case WebGLRenderingContext.FLOAT_VEC3:
+                return 'uniform3f';
+            case WebGLRenderingContext.FLOAT_VEC4:
+                return 'uniform4f';
+
+            case WebGLRenderingContext.INT:
+                return 'uniform1i';
+            case WebGLRenderingContext.INT_VEC2:
+                return 'uniform2i';
+            case WebGLRenderingContext.INT_VEC3:
+                return 'uniform3i';
+            case WebGLRenderingContext.INT_VEC4:
+                return 'uniform4i';
+
+            // case WebGLRenderingContext.BOOL:
+            //     return WebGLRenderingContext.uniform1f;
+            // case WebGLRenderingContext.BOOL_VEC2:
+            //     return WebGLRenderingContext.uniform1f;
+            // case WebGLRenderingContext.BOOL_VEC3:
+            //     return WebGLRenderingContext.uniform1f;
+            // case WebGLRenderingContext.BOOL_VEC4:
+            //     return WebGLRenderingContext.uniform1f;
+
+            case WebGLRenderingContext.FLOAT_MAT2:
+                return 'uniformMatrix2fv';
+            case WebGLRenderingContext.FLOAT_MAT3:
+                return 'uniformMatrix3fv';
+            case WebGLRenderingContext.FLOAT_MAT4:
+                return 'uniformMatrix4fv';
+
+            case WebGLRenderingContext.SAMPLER_2D:
+                return 'uniform1i';
+            case WebGLRenderingContext.SAMPLER_CUBE:
+                return 'uniform1i';
         }
     }
 }
