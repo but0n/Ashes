403cc19451620f44feb5d908e3b2c192c11d5af7
but0n <but0n@icloud.com>
2018-11-26 00:48:25 +0800

    chore

--- src/ECS/entity.ts
+++ src/ECS/entity.ts
@@ -1,9 +1,17 @@
 export class Entity {
     static createEntity() {
-        return document.createElement('ash-entity');
+        let gameObject: any = document.createElement('ash-entity');
+        gameObject.components = {};
+        return gameObject;
     }
 
     static findEntity(selector:string) {
         return document.querySelectorAll(selector);
+    }
+
+    static addComponent(entity, component: Object) {
+        let componentName = component.constructor.name;
+        entity.components[componentName] = component;
+        entity.dataset[componentName] = '';
     }
 }
