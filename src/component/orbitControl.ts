61caef484915beb551c20a15ba268ca0f027f064
but0n <but0n@icloud.com>
2018-12-11 19:38:32 +0800

    simple movement, not correct yet

--- src/component/orbitControl.ts
+++ src/component/orbitControl.ts
@@ -3,6 +3,7 @@ import { Entity } from "../ECS/entityMgr";
 import { Camera } from "../camera";
 import { Transform } from "../transform";
 import { System } from "../ECS/system";
+import { vec3, mat4, vec4 } from "../../node_modules/gl-matrix/lib/gl-matrix";
 
 export class OrbitControl {
     deltaX: number;
@@ -13,15 +14,13 @@ export class OrbitControl {
     }
     moveHandler = (e) => {
         let {movementX, movementY} = e;
-        this.deltaX = movementX;
-        this.deltaY = movementY;
+        this.deltaX = -movementX * System.deltaTime;
+        this.deltaY = -movementY * System.deltaTime;
         let camera = this.entity.components.Camera as Camera;
         let trans = this.entity.components.Transform as Transform;
         camera.isDirty = true;
-        trans.translate[0] += movementX * System.deltaTime;
-        trans.translate[2] += movementY * System.deltaTime;
-        camera.lookAt[0] += movementX * System.deltaTime;
-        camera.lookAt[2] += movementY * System.deltaTime;
+        vec3.rotateX(trans.translate, trans.translate, camera.lookAt, this.deltaY);
+        vec3.rotateY(trans.translate, trans.translate, camera.lookAt, this.deltaX);
         console.log(movementX, movementY, camera);
     }
     static init(screen: HTMLElement, controler: OrbitControl) {
