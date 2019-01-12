15ec9223e47596055df17a43c9a447e529637864
but0n <but0n@icloud.com>
2019-01-12 19:30:47 +0800

    add normal

--- src/mesh/quadMesh.ts
+++ src/mesh/quadMesh.ts
@@ -3,10 +3,10 @@ import { Mesh, bufferView, Accessor } from "./mesh";
 export class QuadMesh extends Mesh {
     constructor() {
         let meshVBO = new Float32Array([
-            -1, 1, 0.0, 0.0, 1.0,
-            1, 1, 0.0, 1.0, 1.0,
-            1, -1, 0.0, 1.0, 0.0,
-            -1, -1, 0.0, 0.0, 0.0
+            -1, 1, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0,
+            1, 1, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0,
+            1, -1, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0,
+            -1, -1, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0
         ]);
         let meshEBO = new Uint16Array([
             0, 1, 2,
@@ -15,7 +15,7 @@ export class QuadMesh extends Mesh {
         let vbo = new bufferView(meshVBO.buffer, {
             byteOffset: meshVBO.byteOffset,
             byteLength: meshVBO.byteLength,
-            byteStride: 5*4,
+            byteStride: 8*4,
             target: WebGL2RenderingContext.ARRAY_BUFFER
         });
         let ebo = new bufferView(meshEBO.buffer, {
@@ -35,10 +35,17 @@ export class QuadMesh extends Mesh {
         let uv = new Accessor({
             bufferView: vbo,
             componentType: WebGL2RenderingContext.FLOAT,
-            byteOffset: 3*4,
+            byteOffset: 3 * 4,
             type: "VEC2",
             count: 4
         }, 'TEXCOORD_0');
+        let normal = new Accessor({
+            bufferView: vbo,
+            componentType: WebGL2RenderingContext.FLOAT,
+            byteOffset: 5 * 4,
+            type: "VEC3",
+            count: 4
+        }, 'NORMAL');
         let indices = new Accessor({
             bufferView: ebo,
             componentType: WebGL2RenderingContext.UNSIGNED_SHORT,
@@ -46,7 +53,7 @@ export class QuadMesh extends Mesh {
             type: "SCALAR",
             count: 6
         });
-        super([position, uv], indices);
+        super([position, uv, normal], indices);
 
     }
 }
