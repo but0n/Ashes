847b1ea73fb0e107f617f6893ec929d39a81ff67
but0n <but0n@icloud.com>
2019-02-19 18:28:01 +0800

    update

--- src/example.ts
+++ src/example.ts
@@ -7,54 +7,144 @@ import { Screen } from "./webgl2/screen";
 import { QuadMesh } from "./mesh/quadMesh";
 import { MeshRenderer } from "./meshRenderer";
 import { OrbitControl } from "./component/orbitControl";
+import { Bloom } from "./filter/bloom";
+import { Filter } from "./filter";
+import { Shader } from "./shader";
+import { Material } from "./material";
+
+
+
+// BoomBox
+let gltf = 'gltfsamples/BoomBox.glb';
+
 
 export class Example {
 
     static async run() {
-        let gltf = '/gltfsamples/BoxTextured/BoxTextured.gltf';
-        // gltf = '/static/gltfsamples/Suzanne/Suzanne.gltf';
-        gltf = '/gltfsamples/toon_shader_tutorial_files/scene.gltf';
-        // gltf = '/static/gltfsamples/nierautomata__2b/scene.gltf';
-        // gltf = '/static/gltfsamples/sketchfab_3d_editor_challenge_littlest_tokyo/scene.gltf';
-        // gltf = '/static/gltfsamples/hylian_shield/scene.gltf';
-        gltf = '/gltfsamples/FlightHelmet/glTF/FlightHelmet.gltf';
-        // gltf = '/static/gltfsamples/futuristic_safe/scene.gltf';
+        let progressBar: any = document.querySelector('#progress');
+
 
         let screen = new Screen('#screen');
-
-        let scene = EntityMgr.create('root');
-        let gltfroot = await Asset.loadGLTF(gltf, screen);
-        scene.appendChild(gltfroot);
-        let root = gltfroot.components.Transform as Transform;
-        root.scale[0] = root.scale[1] = root.scale[2] = 0.1;
+        screen.bgColor = [0.2, 0.2, 0.2, 1];
+        screen.bgColor = [1,1,1,1];
 
 
+        let load = new demoLoad(screen);
+        screen.attachFilter(load);
+        Asset.taskObserve = (finished, total) => {
+            let p = finished / total;
+            // progressBar.value = p;
+            load.cur = p;
+        }
+        // Filters
+        Bloom.initFilters(screen, 0.6, 80, 1.2);
+        // screen.attachFilter(new Vignetting(screen));
+
+
+
+        let scene = EntityMgr.create('root - (Click each bar which has yellow border to toggle visible)');
+        document.querySelector('body').appendChild(scene);
+
+        let skybox = await Asset.loadCubemap('res/envmap/GoldenGateBridge2/');
+
+
+
+        // Camera and controls
         let mainCamera = EntityMgr.create('camera');
-        let cameraTrans = mainCamera.components.Transform as Transform;
-        let cam: Camera = EntityMgr.addComponent(mainCamera, new Camera(screen.width / screen.height));
-        vec3.set(cameraTrans.translate, 0, 0, 10);
+        let cameraTrans = mainCamera.components.Transform;
+        let cam = EntityMgr.addComponent(mainCamera, new Camera(screen.width / screen.height));
+        vec3.set(cameraTrans.translate, 0, 10, 10);
 
         scene.appendChild(mainCamera);
-        let control = new OrbitControl(screen, mainCamera);
-
-        // BRDF test
-        let brdf = await Asset.LoadMaterial('brdf');
-
-        let quad = EntityMgr.create('test-quad');
-        let qmesh = new QuadMesh();
-        let quadMR = new MeshRenderer(screen, qmesh, brdf);
-        EntityMgr.addComponent(quad, quadMR);
-        console.log(quadMR);
-        quad.components.Transform.translate[0] = 2;
-
-        // scene.appendChild(quad);
-
-        document.querySelector('body').appendChild(scene)
+        EntityMgr.addComponent(mainCamera, new OrbitControl(screen, mainCamera));
 
 
+        // Model
+
+        let gltfroot = await Asset.loadGLTF(gltf, screen, skybox);
+        // scene.appendChild(gltfroot);
+
+
+
+
+        let root = gltfroot.components.Transform;
+        // root.translate[1] = yoffset || 0;
+        vec3.scale(root.scale, root.scale, 200)
+
+
+
+        progressBar.parentElement.style.display = 'none';
 
     }
-};
+}
+
+
+
+class demoLoad extends Filter {
+    cur;
+    stop;
+    constructor(screen: Screen) {
+        let macro = {
+        }
+        let shader = new Shader(loading_vs, loading_fs, macro);
+        super(screen, shader);
+        this.cur = 0;
+        this.stop = false;
+        Material.setUniform(this.material, 'cur', this.cur);
+        let d = 0;
+        let loop = () => {
+            d += (1 / 60 / 3);
+            Material.setUniform(this.material, 'cur', d);
+            if(!this.stop)
+                requestAnimationFrame(loop);
+        }
+        loop();
+    }
+
+
+}
+
+let loading_vs = `
+attribute vec3 POSITION;
+attribute vec2 TEXCOORD_0;
+
+varying vec2 uv;
+varying vec4 pos;
+
+void main() {
+  uv = TEXCOORD_0;
+  vec4 position = vec4(POSITION, 1);
+  pos = position;
+  gl_Position = position;
+}
+`;
+
+let loading_fs = `
+precision mediump float;
+uniform sampler2D base;
+uniform float cur;
+
+varying vec2 uv;
+varying vec4 pos;
+
+void main() {
+    vec4 base = texture2D(base, uv);
+    float prog = abs(uv.y - 0.5) * 2.;
+
+    // Middle out
+
+
+    if(cur < prog) {
+        gl_FragColor = vec4(vec3(0), 1);
+    } else {
+        gl_FragColor = base;
+    }
+}
+`;
+
+
+
+
+
 
 Example.run();
-
