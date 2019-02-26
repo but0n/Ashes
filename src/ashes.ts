940a63afe14906731da74b806ff20742980dd672
but0n <but0n@icloud.com>
2019-02-26 13:34:32 +0800

    export ComponentSystem #18

--- src/ashes.ts
+++ src/ashes.ts
@@ -2,6 +2,7 @@ export { Screen } from "./webgl2/screen";
 export { Asset } from "./asset";
 // ECS
 export { EntityMgr } from "./ECS/entityMgr";
+export { ComponentSystem } from "./ECS/component";
 export { System } from "./ECS/system";
 // Math
 export { vec3, vec4, mat4, quat } from "./math";
