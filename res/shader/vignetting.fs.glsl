bcd4bc34983b8523ce57b28f397b65f3ec360eb9
but0n <but0n@icloud.com>
2019-03-15 15:28:29 +0800

    filter standard macros

--- res/shader/vignetting.fs.glsl
+++ res/shader/vignetting.fs.glsl
@@ -2,9 +2,10 @@ precision highp float;
 uniform sampler2D base;
 
 void main(){
-    // float mask=1.-dot(pos,pos)*FACTOR;
-    // mask=clamp(.5+(mask-.5)*HARDNESS,0.,1.);
-    // vec4 color=texture2D(base,uv);
-    // gl_FragColor=mix(vec4(vec3(0),1),color,mask);
-    gl_FragColor = vec4(mod(gl_FragCoord.xy, 1.),, 1);
+    vec2 uv = gl_FragCoord.xy / oResolution;
+    vec2 pos = uv * 2. - 1.;
+    float mask=1.-dot(pos,pos)*FACTOR;
+    mask=clamp(.5+(mask-.5)*HARDNESS,0.,1.);
+    vec4 color=texture2D(base,uv);
+    gl_FragColor=mix(vec4(vec3(0),1),color,mask);
 }
--- src/filter/vignetting.ts
+++ src/filter/vignetting.ts
@@ -8,12 +8,15 @@ export class Vignetting extends Filter {
         super(screen, new Shader(vig_vs, vig_fs, {
             FACTOR:     `float(${factor})`,
             HARDNESS:   `float(${hardness})`,
+            // Addition macros:
+            screenSize:     `vec2(${screen.pow2width}, ${screen.pow2height})`,
+            iResolution:    `vec2(${screen.pow2width}, ${screen.pow2height})`,
+            oResolution:    `vec2(${screen.pow2width}, ${screen.pow2height})`,
         }));
     }
 
 
 }
-// FIXME: texcoords
 let vig_vs = glsl.vignetting.vs;
 
 let vig_fs = glsl.vignetting.fs;
