6e94a07f764fc6e438b498c69918ea09c15c561a
but0n <but0n@icloud.com>
2019-06-09 09:31:00 +0800

    feat vec3 pool

--- src/math.ts
+++ src/math.ts
@@ -1,1 +1,16 @@
 export { vec2, vec3, vec4, mat4, quat } from "../node_modules/gl-matrix/lib/gl-matrix";
+import { vec2, vec3, vec4, mat4, quat } from "../node_modules/gl-matrix/lib/gl-matrix";
+
+export class vec3Pool {
+    private static pool = [];
+    static create() {
+        if(this.pool.length) {
+            return this.pool.pop();
+        }
+        return vec3.create();
+    }
+    static delete(vec) {
+        vec3.set(vec, 0, 0, 0);
+        this.pool.push(vec);
+    }
+}
