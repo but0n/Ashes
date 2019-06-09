11126a0e9cfcf863a2e8c3decd181dbed2f47b22
but0n <but0n@icloud.com>
2019-06-09 09:30:22 +0800

    update glsl

--- src/glsl.ts
+++ src/glsl.ts
@@ -2,8 +2,12 @@ import basic_vs from "../res/shader/basic.vs.glsl"
 import basic_fs from "../res/shader/basic.fs.glsl"
 import stylize_vs from "../res/shader/stylize.vs.glsl"
 import stylize_fs from "../res/shader/stylize.fs.glsl"
+import stylize2_vs from "../res/shader/stylize2.vs.glsl"
+import stylize2_fs from "../res/shader/stylize2.fs.glsl"
 import vignetting_vs from "../res/shader/vignetting.vs.glsl"
 import vignetting_fs from "../res/shader/vignetting.fs.glsl"
+import line_vs from "../res/shader/line.vs.glsl"
+import line_fs from "../res/shader/line.fs.glsl"
 
 export let glsl = {
     basic: {
@@ -14,8 +18,16 @@ export let glsl = {
         vs: stylize_vs,
         fs: stylize_fs,
     },
+    sylize2: {
+        vs: stylize2_vs,
+        fs: stylize2_fs,
+    },
     vignetting: {
         vs: vignetting_vs,
         fs: vignetting_fs,
+    },
+    line: {
+        vs: line_vs,
+        fs: line_fs,
     },
 };
